a3fec34480410fd4ba075a6a4450b4fa8e8611bf
David A. Andrews <andrews@udallas.edu>
2017-02-22 16:32:23 -0600

    Started to add the code for the output pipe to bc

--- exec-pipe.c
+++ exec-pipe.c
@@ -3,11 +3,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[], char *envp[]) {
   
-  int fifo[2];
-  if ( pipe(fifo) < 0 ) {
+  int ififo[2];
+  if ( pipe(ififo) < 0 ) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+
+  int ofifo[2];
+  if ( pipe(ofifo) < 0 ) {
     perror("pipe");
     exit(EXIT_FAILURE);
   }
@@ -19,27 +26,38 @@ int main(int argc, char *argv[], char *envp[]) {
   }
 
   if ( pid == 0 ) {
-    if ( dup2(fifo[0], 0) < 0 ) {
+    if ( dup2(ififo[0], 0) < 0 ) {
       perror("dup2");
       exit(EXIT_FAILURE);
     }
 
-    close(fifo[1]);
+    if ( dup2(ofifo[1], 1) < 0 ) {
+      perror("dup2");
+      exit(EXIT_FAILURE);
+    }
 
-    char *args[] = {"wc", NULL};
+    close(ififo[1]);
+    close(ofifo[0]);
+
+    char *args[] = {"bc", NULL};
 
     execve("/usr/bin/bc", args, envp);
     perror("exec");
     exit(EXIT_FAILURE);
   }
 
+  char wstr[100];
+  char rstr[100];
   for ( int i = 0 ; i < 100 ; i++ ) {
-    if ( write(fifo[1], "6 * 9\n", 6) < 6 ) {
+    
+    sprintf(bcstr, "6 * %d\n", i);
+    int slen = strlen(bcstr);
+    if ( write(ififo[1], bcstr, slen) < slen ) {
       perror("write");
     }
   }
   
-  close(fifo[1]);
+  close(ififo[1]);
   
   wait(NULL);
 
